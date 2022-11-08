36b266eb4fb239b7d0285418309ab03b9f95599c
Víctor Cançado <victor.cancado@gmail.com>
2022-11-07 22:00:28 -0300

    Table

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let Reader = require("./Reader");
 let Processor = require("./Processor");
+let Table = require("./Table");
 
 let leitor = new Reader();
 
 async function main() {
   let dados = await leitor.Read("./users.csv");
   let dadosProcessados = Processor.Process(dados);
+
+  let usuarios = new Table(dadosProcessados);
+
+  console.log(usuarios.rows)
 }
 
 main();
