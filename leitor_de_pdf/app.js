d12d05cb78009363e27d54ae461bae2693129d09
Víctor Cançado <victor.cancado@gmail.com>
2022-11-10 22:08:24 -0300

    Writer

--- leitor_de_pdf/app.js
+++ leitor_de_pdf/app.js
@@ -1,8 +1,11 @@
 let Reader = require("./Reader");
+let Writer = require("./Writer");
 let Processor = require("./Processor");
 let Table = require("./Table");
 let HtmlParser = require("./HtmlParser");
+
 let leitor = new Reader();
+let escritor = new Writer();
 
 async function main() {
   let dados = await leitor.Read("./users.csv");
@@ -11,6 +14,8 @@ async function main() {
   let usuarios = new Table(dadosProcessados);
   
   var html = await HtmlParser.Parse(usuarios);
+
+  escritor.Write("meuarquivogerado.html", html);
 
   console.log(html);
 }
